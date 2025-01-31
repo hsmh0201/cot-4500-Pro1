9049d8639b1ba03dcbc00b2f111fe6756f89eaa1
hsmh0201 <is375130@ucf.edu>
2025-01-31 17:14:15 +0000

    approximation

--- main/assignment_1.py
+++ main/assignment_1.py
@@ -1,1 +1,22 @@
-print("hello wolrd")
+## Approximation Algorithm 
+def root_two():
+    x0 = 1.5
+    tol = 0.000001
+
+    iteration_count = 0
+    diff = x0
+    x = x0
+
+    print(f"{iteration_count} : {x}")
+
+    while diff >= tol:
+        iteration_count += 1
+        y = x
+        x = (x / 2) + (1 / x)
+        print(f"{iteration_count} : {x}")
+
+        diff = abs(x - y)
+
+    print(f"\nConvergence after {iteration_count} iterations")
+
+root_two()
